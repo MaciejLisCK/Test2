72a384207d2be1fd82e1c61a4c325ae75cc1ed82
Maciej Lis <mail@maciejlis.com>
2016-11-26 17:47:26 +0100

    Add project files.

---
+++ WebApplication95/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication95.MvcApplication" Language="C#" %>
---
+++ WebApplication95/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication95.Models
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
